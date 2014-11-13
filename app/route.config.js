6cacfbbf2faf2a4f3caa742765ebcc1bb647ef39
Greg Fedirko <gjfedirko@gmail.com>
2014-11-12 20:02:33 -0800

    move all html to shell.html template

---
+++ app/route.config.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+  angular
+    .module('app')
+    .config(function Router($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('main');
+
+      $stateProvider
+        .state('main', {
+          url: '/main',
+          templateUrl: 'app/layout/shell.html'
+        });
+    });
+
+})();
