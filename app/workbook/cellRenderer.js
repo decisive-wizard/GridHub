3909b66dbba19d1155a5bd27cea08b2b2a77df56
Nick Stefan <NickStefan12@gmail.com>
2014-11-12 19:17:20 -0800

    basic cellRenderer

--- app/workbook/cellRenderer.js
+++ app/workbook/cellRenderer.js
@@ -1,26 +1,38 @@
 
-var cellRenderer = function (instance, TD, row, col, prop, value, cellProperties) {
+// This render function is based on line 4892 TextRenderer from 
+// handsontable.full.js. original function passes the arg 'value' 
+// straight to the escaped val and then to 
+// Handsontable.Dom.fastInnerText(TD,escaped). Here we are instead going to
+// take a 'value' as an object and pass the correct aspects to the renderer
+// rather than modify handsontable src, this gets passed to handsontable
+// constructor via the Worksheet class
+
+var cellRenderer = function (instance, TD, row, col, prop, valueObj, cellProperties) {
 
   Handsontable.renderers.cellDecorator.apply(this, arguments);
 
-  if (!value && cellProperties.placeholder) {
-    value = cellProperties.placeholder;
+  if (!valueObj && cellProperties.placeholder) {
+    valueObj = cellProperties.placeholder;
   }
 
-  var escaped = Handsontable.helper.stringify(value);
+  addStyles(TD,valueObj);
+  var cellText = viewSelectProperty(valueObj);
 
-  if (cellProperties.rendererTemplate) {
-    Handsontable.Dom.empty(TD);
-    var TEMPLATE = document.createElement('TEMPLATE');
-    TEMPLATE.setAttribute('bind', '{{}}');
-    TEMPLATE.innerHTML = cellProperties.rendererTemplate;
-    HTMLTemplateElement.decorate(TEMPLATE);
-    TEMPLATE.model = instance.getSourceDataAtRow(row);
-    TD.appendChild(TEMPLATE);
-  }
-  else {
-    Handsontable.Dom.fastInnerText(TD, escaped);
-  }
+  // escape and select the property value to be displayed in the cell view to be displayed
+  var escaped = Handsontable.helper.stringify(cellText);
+
+  Handsontable.Dom.fastInnerText(TD, escaped);
 
 };
+
+function addStyles(TD,valueObj){
+
+}
+
+function viewSelectProperty(valueObj){
+ if (typeof valueObj === 'string'){
+  return valueObj;
+ }
+
+}
 
