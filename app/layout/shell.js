2b74730733cfdcbe0a729228b17c54372b7bdb2e
Greg Fedirko <gjfedirko@gmail.com>
2014-11-13 13:26:11 -0800

    add shell controller

---
+++ app/layout/shell.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.layout')
+    .controller('ShellController', ShellController);
+
+    ShellController.$inject = ['$scope'];
+
+    function ShellController ($scope) {
+
+    }
+
+})();
