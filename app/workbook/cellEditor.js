6d4a9c145a4d496390ce55d7fa352699c45ad72d
Nick Stefan <NickStefan12@gmail.com>
2014-11-15 20:33:52 -0800

    RuleJS formulas mixed in with our cellRenderer

--- app/workbook/cellEditor.js
+++ app/workbook/cellEditor.js
@@ -3,13 +3,18 @@ var cellEditor = Handsontable.editors.TextEditor.prototype.extend();
 // load the initial value in the editor field
 cellEditor.prototype.setValue = function(cellObj){
   this.cellObj = cellObj;
-  this.TEXTAREA.value = cellObj.value;
+  this.TEXTAREA.value = cellObj.formula ? cellObj.formula : cell.value;
 };
 
 // retrieve what the user has entered
 cellEditor.prototype.getValue = function(){
   // .trim() is extended onto String inside Handsontable
-  this.cellObj.value = String.prototype.trim.call(this.TEXTAREA.value);
+  var str = String.prototype.trim.call(this.TEXTAREA.value);
+  if (str && str[0] === '='){
+    this.cellObj.formula = str;
+  } else {
+    this.cellObj.value = str;
+  }
   return this.cellObj;
 };
 
