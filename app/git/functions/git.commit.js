e17a5b3ef6fa46216a88bf1c1ba96ef23db64cdb
Greg Fedirko <gregfedirko@users.noreply.github.com>
2014-11-19 11:40:02 -0800

    null

---
+++ app/git/functions/git.commit.js
@@ -0,0 +1,17 @@
+var gift = require('gift');
+var path = require('path');
+var fs = require('fs');
+
+module.exports = function(rootFilePath, message) {
+  var resolvedPath = path.resolve(rootFilePath);
+
+  var repo = gift(resolvedPath);
+
+  repo.commit(message, {all: true, amend: false, author: 'John Heroy'}, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+
+
+};
